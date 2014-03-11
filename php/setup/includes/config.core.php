7afa3d57f2ab5511208e9441fb11cb0563196200
Wesley Hearn <whearn@redhat.com>
2014-03-11 14:20:24 -0400

    Remove setup directory

--- php/setup/includes/config.core.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (!defined('MODX_CORE_PATH')) define('MODX_CORE_PATH', MODX_INSTALL_PATH . 'core/');
-if (!defined('MODX_CONFIG_KEY')) define('MODX_CONFIG_KEY', 'config');
-define ('MODX_SETUP_KEY', '@traditional@');
--- php/setup/includes/upgrades/mysql/2.2.6-pl.php
+++
@@ -1,3 +0,0 @@
-<?php
-/* disable allow_tags_in_post System Setting */
-include dirname(dirname(__FILE__)).'/common/2.2-disable-allowtagsinpost.php';
